851e95527ddf18f6e65f7aab6c96f64ec3629295
Matt Goucher <matt@mattgoucher.com>
2016-09-14 00:47:47 -0700

    build publishes to npm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,11 @@
+git config --global user.email "matt@mattgoucher.com"
+git config --global user.name "Matt Goucher"
+
 # Generate a release commit
 ./node_modules/.bin/standard-version patch -m "chore(release): version %s build ${CIRCLE_BUILD_NUM} [ci skip]"
 
 # Push tags to github
-- git push --follow-tags origin ${CIRCLE_BRANCH}
+git push --follow-tags origin ${CIRCLE_BRANCH}
 
 # Publish to NPM
-# - npm publish
+npm publish
