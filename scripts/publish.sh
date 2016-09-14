43c857fde07ed2d4effa31f575aa313578640069
Matt Goucher <matt@mattgoucher.com>
2016-09-14 00:39:50 -0700

    add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,8 @@
+# Generate a release commit
+./node_modules/.bin/standard-version patch -m "chore(release): version %s build ${CIRCLE_BUILD_NUM} [ci skip]"
+
+# Push tags to github
+- git push --follow-tags origin ${CIRCLE_BRANCH}
+
+# Publish to NPM
+# - npm publish
